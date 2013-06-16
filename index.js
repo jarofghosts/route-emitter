124f45a83bf065a4863a058d31d545ba4561940f
jesse keane <me@jessekeane.me>
2013-06-16 22:29:33 +0400

    spooky scary skeletons

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
-var http = require('http');
+var util = require('util'),
+  events = require('events');
+
+function Router() {
+  
+  this.routes = {
+    "get": {},
+    "put": {},
+    "post": {},
+    "delete": {}
+  };
+
+  this.route = function (req, res) {
+    
+  };
+}
