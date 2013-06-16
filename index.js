845227259999942d2cd1666efb39b904fc6eab7b
jesse keane <me@jessekeane.me>
2013-06-16 22:50:41 +0400

    this is kind of making sense

--- index.js
+++ index.js
@@ -6,11 +6,32 @@ function Router() {
   this.routes = {};
 
   this.listen = function (method, path, name, callback) {
+    
+    method = method.toLowerCase();
+    
+    if (typeof name != 'string') {
+      callback = name;
+      name = method + ':' + path;
+    }
+
+    if (this.routes[method] == undefined) {
+      this.routes[method] = {};
+    }
+
+    this.routes[method].path = path;
+    this.routes[method].callback = callback;
+    
+    if (callback != undefined) {
+      this.on(name, callback);
+    }
   };
 
   this.route = function (req, res) {
     var method = req.method.toLowerCase();
     
+    this.routes['before:*'] && executeRoutes(this.routes['before:*']);
+    this.routes['before:' + method] && executeRoutes(this.routes['before:' + method]);
+
   };
 }
 
