aa7cba3bdbd7373cc8ba9d8b4cd35dbad61f52a9
jesse keane <me@jessekeane.me>
2013-09-01 05:44:26 +0400

    for clarity

--- index.js
+++ index.js
@@ -3,11 +3,9 @@ var util = require('util'),
     EventEmitter = require('events').EventEmitter;
 
 function Router() {
-  
   if (!this instanceof Router) return new Router();
-
   this.routes = {};
-
+  return this;
 }
 
 util.inherits(Router, EventEmitter);
@@ -21,13 +19,11 @@ Router.prototype.listen = function (method, path, name, callback) {
     name = method + ':' + path;
   }
 
-  if (this.routes[method] == undefined) {
-    this.routes[method] = {};
-  }
+  if (!this.routes[method]) this.routes[method] = {};
 
-  path = (path == '*' || /^\//.test(path) || /^:/.test(path)) ? path : '/' + path;
+  if (path != '*' && !/^\//.test(path) && !/^:/.test(path)) path = '/' + path;
 
-  this.routes[method][path] = { 
+  this.routes[method][path] = {
     name: name
   };
   
@@ -41,16 +37,17 @@ Router.prototype.route = function (req, res) {
 
   var method = req.method.toLowerCase(),
       url = urlParse(req.url),
-      hasStar = !!this.routes['*'];
+      hasStar = !!this.routes['*'],
+      hasMethod = !!this.routes[method];
 
   hasStar && this.routes['*'][':before'] && this.emit(this.routes['*'][':before'].name, req, res);
-  this.routes[method] && this.routes[method][':before'] && this.emit(this.routes[method][':before'].name, req, res);
+  hasMethod && this.routes[method][':before'] && this.emit(this.routes[method][':before'].name, req, res);
 
-  if ((this.routes[method] && this.routes[method][url.pathname] && !this.emit(this.routes[method][url.pathname].name, req, res))
-    || (!this.routes[method] || !this.routes[method][url.pathname])) {
+  if ((hasMethod && this.routes[method][url.pathname] && !this.emit(this.routes[method][url.pathname].name, req, res))
+    || (!hasMethod || !this.routes[method][url.pathname])) {
       
-    if ((this.routes[method] && this.routes[method]['*'] && !this.emit(this.routes[method]['*'].name, req, res))
-      || (!this.routes[method] || !this.routes[method]['*'])) {
+    if ((hasMethod && this.routes[method]['*'] && !this.emit(this.routes[method]['*'].name, req, res))
+      || (!hasMethod || !this.routes[method]['*'])) {
       
       if ((hasStar && this.routes['*']['*'] && !this.emit(this.routes['*']['*'].name, req, res))
         || (!hasStar || !this.routes['*']['*'])) {
@@ -63,7 +60,7 @@ Router.prototype.route = function (req, res) {
 
   }
 
-  this.routes[method] && this.routes[method][':after'] && this.emit(this.routes[method][':after'].name, req, res);
+  hasMethod && this.routes[method][':after'] && this.emit(this.routes[method][':after'].name, req, res);
   hasStar && this.routes['*'][':after'] && this.emit(this.routes['*'][':after'].name, req, res);
 
 };
