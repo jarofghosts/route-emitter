5c315a72d631ee272c8f8aea12f6e6d6040ec78e
jesse keane <me@jessekeane.me>
2013-11-16 19:30:06 -0800

    remove unnecessary before after crap

--- README.md
+++ README.md
@@ -9,61 +9,47 @@ Dead-simple routing for node.
 ````js
 var http = require('http'),
   Router = require('route-emitter').Router,
-  router = new Router();
+  router = new Router()
 
 router.listen('get', '/', function (req, res) {
-  res.end('Hello, world');
-});
+  res.end('Hello, world')
+})
 
 // listen for any http verb!
 router.listen('post', '/blog', function (req, res) {
-  res.end('BLOG CREATED!');
-});
-
-// special paths ':before' and ':after' can be applied to any verb!
-router.listen('put', ':before', function (req, res) {
-  res.end('NO PUT ALLOWED.');
-});
-
-// they can also be applied with a wildcard!
-router.listen('*', ':after', function (req, res) {
-  hitCounter++;
-});
+  res.end('BLOG CREATED!')
+})
 
 // or you can catch 404s
 router.listen('*', '*', function (req, res) {
-  res.writeHead(404);
-  res.end('PAGE NOT FOUND!');
-});
+  res.writeHead(404)
+  res.end('PAGE NOT FOUND!')
+})
 
 // ...or verb-specific 404s
 router.listen('put', '*', function (req, res) {
-  res.writeHead(404);
-  res.end('RESOURCE NOT FOUND!');
-});
+  res.writeHead(404)
+  res.end('RESOURCE NOT FOUND!')
+})
 
 // create a listener with named emitter!
-router.listen('delete', '/blog', 'deleteThatBlog');
+router.listen('delete', '/blog', 'deleteThatBlog')
 
 // react to the emit!
 router.on('deleteThatBlog', function (req, res) {
-  res.end('BLOG DELETED');
-});
+  res.end('BLOG DELETED')
+})
 
 http.createServer(function (req, res) {
-  router.route(req, res);
-});
+  router.route(req, res)
+})
 ````
 
 ## other info
 
 Order of operations goes:
-* \*:before
-* (verb):before
-* (verb)/path if found, otherwise (verb)\* if found, otherwise \*\*
-* (verb):after
-* \*:after
+* `(verb)/path` if found, otherwise `(verb)/*` if found, otherwise `*/*`
 
 ## license
 
-BSD
+MIT
--- index.js
+++ index.js
@@ -1,74 +1,67 @@
 var util = require('util'),
     urlParse = require('url').parse,
-    EventEmitter = require('events').EventEmitter;
+    EventEmitter = require('events').EventEmitter
 
 function Router() {
-  if (!this instanceof Router) return new Router();
-  this.routes = {};
-  return this;
+  if (!this instanceof Router) return new Router()
+  this.routes = {}
+  return this
 }
 
-util.inherits(Router, EventEmitter);
+util.inherits(Router, EventEmitter)
 
 Router.prototype.listen = function (method, path, name, callback) {
     
-  method = method.toLowerCase();
+  method = method.toLowerCase()
   
   if (typeof name != 'string') {
-    callback = name;
-    name = method + ':' + path;
+    callback = name
+    name = method + ':' + path
   }
 
-  if (!this.routes[method]) this.routes[method] = {};
+  if (!this.routes[method]) this.routes[method] = {}
 
-  if (path != '*' && !/^\//.test(path) && !/^:/.test(path)) path = '/' + path;
+  if (path != '*' && !/^\//.test(path) && !/^:/.test(path)) path = '/' + path
 
   this.routes[method][path] = {
     name: name
-  };
+  }
   
   if (callback != undefined) {
-    this.on(name, callback);
+    this.on(name, callback)
   }
 
-};
+}
 
 Router.prototype.route = function (req, res) {
 
   var method = req.method.toLowerCase(),
       url = urlParse(req.url),
       hasStar = !!this.routes['*'],
-      hasMethod = !!this.routes[method];
+      hasMethod = !!this.routes[method]
 
-  hasStar && this.routes['*'][':before'] && this.emit(this.routes['*'][':before'].name, req, res);
-  hasMethod && this.routes[method][':before'] && this.emit(this.routes[method][':before'].name, req, res);
-
-  if ((hasMethod && this.routes[method][url.pathname] && !this.emit(this.routes[method][url.pathname].name, req, res))
+  if ((hasMethod && this.routes[method][url.pathname] &&
+    !this.emit(this.routes[method][url.pathname].name, req, res))
     || (!hasMethod || !this.routes[method][url.pathname])) {
       
-    if ((hasMethod && this.routes[method]['*'] && !this.emit(this.routes[method]['*'].name, req, res))
+    if ((hasMethod && this.routes[method]['*'] &&
+      !this.emit(this.routes[method]['*'].name, req, res))
       || (!hasMethod || !this.routes[method]['*'])) {
       
-      if ((hasStar && this.routes['*']['*'] && !this.emit(this.routes['*']['*'].name, req, res))
+      if ((hasStar && this.routes['*']['*'] &&
+        !this.emit(this.routes['*']['*'].name, req, res))
         || (!hasStar || !this.routes['*']['*'])) {
 
-        console.log('no direct route found');
-      
+        process.stdout.write('unrouted ' + method + ' ' + url.pathname)
       }
-
     }
-
   }
-
-  hasMethod && this.routes[method][':after'] && this.emit(this.routes[method][':after'].name, req, res);
-  hasStar && this.routes['*'][':after'] && this.emit(this.routes['*'][':after'].name, req, res);
-
-};
-
-function createRouter() {
-  return new Router();
 }
 
-module.exports.Router = Router;
-module.exports.createRouter = createRouter;
+function createRouter() {
+  return new Router()
+}
+
+module.exports.Router = Router
+module.exports.createRouter = createRouter
 
